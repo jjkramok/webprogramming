7b2a72877c49ec623993c50d4d8ef22889580c66
Tim Hintzbergen <t.a.h.hintzbergen@student.utwente.nl>
2015-09-15 12:41:27 +0200

    next two assignments

---
+++ week2/address.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kramok
+ * Date: 15/9/2015
+ * Time: 12:34 PM
+ */
+$name = "Alwin de Vries";
+$street = "Bomenlaan";
+$housenumber = 12;
+$address = $street . " " . 12;
+print($name . ", " . $address);
---
+++ week2/cartoon.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kramok
+ * Date: 15/9/2015
+ * Time: 12:22 PM
+ */
+print("Cartoon characters I like: ");
+$cartchar1 = "Donald";
+print($cartchar1);
+$cartchar1 = "Jerr";
+print($cartchar1);
+$cartchar2 = "Major Kusanagi";
+print(" en ");
+print($cartchar2);
+?>
