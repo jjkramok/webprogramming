fda2acfb07e1110ddbd85defd53f4e34818324ad
Tim Hintzbergen <t.a.h.hintzbergen@student.utwente.nl>
2015-09-15 13:12:31 +0200

    started on assignment 12

---
+++ week2/birthday.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kramok
+ * Date: 15/9/2015
+ * Time: 1:09 PM
+ */
+
+print("Who types? Me, you or he\n");
+$person = trim(fgets(STDIN));
+$birthday = "16 April";
+print ($person);
+if (…) {
+    print (…);
+} else {
+    if (…) {
+        print (…);
+ } else {
+        print (…);
+ }
+}
+print (" has his birthday on " . $birthday);
+?>
