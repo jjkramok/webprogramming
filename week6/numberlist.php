bee2d478c0f099853f879d0c47c0bb8b6e3720ba
Tim Hintzbergen <t.a.h.hintzbergen@student.utwente.nl>
2015-10-14 13:25:30 +0200

    hate

--- week6/numberlist.php
+++ week6/numberlist.php
@@ -4,15 +4,21 @@
     <h2> Numberlist </h2>
     <?php
     if (isset($_GET["Verwerk"])) {
-
-        $numberlist = explode(" ", $_GET["list"]);
-        ?>
-        <?php print("Som is " . array_sum($numberlist) . "\n"); ?> <br>
-        <?php print("Aantal is " . count($numberlist) . "\n"); ?> <br>
-        <?php print("Minimum is " . min($numberlist) . "\n"); ?> <br>
-        <?php print("Maximum is " . max($numberlist) . "\n"); ?> <br>
+        $raw_list = "";
+        $raw_list = $_GET["list"];
+        $refined_raw_list = str_replace(".", ",", $raw_list);
+        print_r($refined_raw_list);
+        $number_list = explode(" ", $raw_list);
+        print_r($number_list);
+        ?> <br>
+        <?php print("Som is " . array_sum($number_list) . "\n"); ?> <br>
+        <?php print("Aantal is " . count($number_list) . "\n"); ?> <br>
+        <?php print("Minimum is " . min($number_list) . "\n"); ?> <br>
+        <?php print("Maximum is " . max($number_list) . "\n"); ?> <br>
 
     <?php
+        #TODO AFTER SHOWING VARIABLE REPLACE THE DOTS
+        print_r($number_list);
     }
     ?>
     <textarea name="list"></textarea>
