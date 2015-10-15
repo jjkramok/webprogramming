221fcc7e68406261cdb9f090ecca87b7a8bbb10f
Tim Hintzbergen <t.a.h.hintzbergen@student.utwente.nl>
2015-10-15 10:41:52 +0200

    finished the last assignment

--- week6/numberlist.php
+++ week6/numberlist.php
@@ -7,18 +7,14 @@
         $raw_list = "";
         $raw_list = $_GET["list"];
         $refined_raw_list = str_replace(".", ",", $raw_list);
-        print_r($refined_raw_list);
         $number_list = explode(" ", $raw_list);
-        print_r($number_list);
         ?> <br>
-        <?php print("Som is " . array_sum($number_list) . "\n"); ?> <br>
-        <?php print("Aantal is " . count($number_list) . "\n"); ?> <br>
-        <?php print("Minimum is " . min($number_list) . "\n"); ?> <br>
-        <?php print("Maximum is " . max($number_list) . "\n"); ?> <br>
+        <?php print("Som is " . str_replace(".", ",", array_sum($number_list)) . "\n"); ?> <br>
+        <?php print("Aantal is " . str_replace(".", ",", count($number_list)) . "\n"); ?> <br>
+        <?php print("Minimum is " . str_replace(".", ",", min($number_list)) . "\n"); ?> <br>
+        <?php print("Maximum is " . str_replace(".", ",", max($number_list)) . "\n"); ?> <br>
 
     <?php
-        #TODO AFTER SHOWING VARIABLE REPLACE THE DOTS
-        print_r($number_list);
     }
     ?>
     <textarea name="list"></textarea>
